1cd34c55b419b9af3930ed51631861c4acc0b20f
nickkom <komarenkonick@gmail.com>
2021-01-11 14:30:33 +0200

    first commit

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+import vueCustomElement from 'vue-custom-element'
 
 Vue.config.productionTip = false
+
+Vue.use(vueCustomElement);
+Vue.customElement("vue-widget", (App.prototype.constructor as any).options)
 
 new Vue({
   render: h => h(App),
